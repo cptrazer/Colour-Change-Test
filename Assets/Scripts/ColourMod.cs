164b61d1d1744c6cbc6d17707e4b98525fecc2b8
cptrazer <alex_dragon@live.com>
2017-10-05 11:58:11 +0200

    Prototype
    Going alright

---
+++ Assets/Scripts/ColourMod.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourMod : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    GameObject GetGameObject;
+
+    int colourState = 0;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+    private void OnMouseDown()
+    {
+
+        if (colourState == 0)
+        {
+            Debug.Log("Colour 0");
+            gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+
+        if (colourState == 1)
+        {
+            Debug.Log("Colour 1");
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (colourState == 2)
+        {
+            Debug.Log("Colour 2");
+            gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+        }
+                
+        if (colourState == 3)
+        {
+            Debug.Log("Colour 3");
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        colourState = (colourState + 1) % 4;
+
+
+    }
+}
