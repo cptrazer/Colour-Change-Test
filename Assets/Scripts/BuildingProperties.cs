5c382567e383bcd0c393178aa73a30af2b94ec4a
cptrazer <alex_dragon@live.com>
2017-10-30 09:35:40 +0100

    new database test
    This version contains database now

---
+++ Assets/Scripts/BuildingProperties.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingProperties : MonoBehaviour {
+
+    //Here there will be different public gameobjects which will be Prefabs. Depending on the colour that player picks the building will add an amount to that stat.
+
+    //Player cycles through the colours on the building and once they are satisfied they press a button and that "builds" the player's starting stats
+    //Or player cycles through colour but each time they change colour it Contradicts the last statement so instead of + 10 to Orange its -10 to Orange but +10 to whatever colour it is now 
+    //Colour Pointer, look at building with a colour pointer selected and paint that building that colour. 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
