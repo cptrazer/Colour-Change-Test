9c6bfad6b2aae6a502dd28ec65ed29470dbd224f
Dagmar <dagmardik9@gmail.com>
2017-11-03 12:48:42 +0100

    with new databse

---
+++ Assets/Scripts/GameScreen/ListsTesting.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ListsTesting : MonoBehaviour {
+
+    //Have two funcstion for each of the buttons. one function for the consquences of the option A while the other is for option B
+    // These funcstions will go through the database and take out the consequences and apply them to the Stats class 
+   
+    public Text descriptionText;
+    public Text optionAButton;
+    public Text optionBButton;
+    private string connectionString;
+
+    public Stats statsClassReference;
+   
+
+    public Button optionA;
+    public Button optionB;
+
+    public int treasuryConsequence;
+    public float happinessConsequence;
+    public float healthinessConsequence;
+    public float infrastructureConsequence;
+
+    private int rowNumber;
+    private int questionindex;
+    private System.Random rng = new System.Random();
+    
+    
+	void Start () {
+
+        //connection to the folder
+        connectionString = "URI=file:" + Application.dataPath + "/statsgame.db";
+
+        GetEvent();
+        GetOptions();
+        // EventAmount();
+        
+        
+          
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Debug.Log(questionindex);
+        Debug.Log("los" + rowNumber);
+
+    }
+
+     public void GetEvent()
+    {
+        using (IDbConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            //opens the connection to the database
+            dbConnection.Open();
+            rowNumber = rng.Next(1, 35);
+
+            using (IDbCommand dbCmd = dbConnection.CreateCommand())
+            {
+                
+                //Selecting everything from the table 'events'
+                string sqlQuery = String.Format("SELECT * FROM events INNER JOIN options on events.id = options.eventid WHERE events.id = ({0}) ", rowNumber);
+                dbCmd.CommandText = sqlQuery;
+
+                
+
+                using (IDataReader reader = dbCmd.ExecuteReader())
+                {
+                        //Debug.Log(reader.GetInt32(2));
+                        reader.Read(); descriptionText.text = reader.GetString(1);
+                    
+                        reader.Close();
+                         dbConnection.Close();
+                }
+            }
+        }
+    }
+
+   void GetOptions()
+    {
+        using (IDbConnection dbConnector = new SqliteConnection(connectionString))
+        {
+            dbConnector.Open();
+            using (IDbCommand dbCommand = dbConnector.CreateCommand())
+            {
+                string sqlQueer = String.Format("SELECT * FROM options WHERE options.eventid = {0};", rowNumber);
+                dbCommand.CommandText = sqlQueer;
+
+                using (IDataReader reader = dbCommand.ExecuteReader()) 
+                {
+
+                    reader.Read();  optionAButton.text = reader.GetString(2);
+
+                    reader.Read(); optionBButton.text = reader.GetString(2);
+
+                    reader.Close();
+                    
+                }
+                dbConnector.Close();
+            }
+        }
+        
+    }
+
+    public void OptionAConsequence()
+    {
+        using (IDbConnection dbConnect = new SqliteConnection(connectionString))
+        {
+            dbConnect.Open();
+            using (IDbCommand dbCommander = dbConnect.CreateCommand())
+            {
+                string sqlQuestion = String.Format("SELECT * FROM options WHERE options.eventid = {0};",rowNumber);
+                dbCommander.CommandText = sqlQuestion;
+
+                using (IDataReader reader = dbCommander.ExecuteReader())
+                {
+
+                    //Treasury Consequence
+                    reader.Read(); treasuryConsequence = reader.GetInt32(3);
+                        if (treasuryConsequence != 0)
+                        {
+                            statsClassReference.Treasury += treasuryConsequence;
+                        }
+                        Debug.Log(treasuryConsequence);
+                    //Happiness Consequence
+                    happinessConsequence = reader.GetInt32(4);
+                        if (happinessConsequence != 0)
+                        {
+                            statsClassReference.Happiness += happinessConsequence;
+                        }
+                        Debug.Log(happinessConsequence);
+                    //Healthiness Consequence
+                    healthinessConsequence = reader.GetInt32(5);
+                        if (healthinessConsequence != 0)
+                        {
+                            statsClassReference.Healthiness += healthinessConsequence;
+                        }
+                    Debug.Log(healthinessConsequence);
+                    //Infrastructure Consequence
+                     infrastructureConsequence = reader.GetInt32(6);
+                        if (infrastructureConsequence != 0)
+                        {
+                            statsClassReference.Infrastructure += infrastructureConsequence;
+                        }
+                    Debug.Log(infrastructureConsequence);
+
+                    reader.Close();
+                }
+                dbConnect.Close();
+            }
+        }
+        GetEvent();
+        GetOptions();
+    }
+
+    //Perhaps coroutine would work.
+
+    public void OptionBConsequences()
+    {
+        using (IDbConnection dbConn = new SqliteConnection(connectionString))
+        {
+            dbConn.Open();
+            using (IDbCommand dbComm = dbConn.CreateCommand())
+            {
+                string sqlQuery = String.Format("SELECT * FROM options WHERE options.eventid = {0};",rowNumber);
+                dbComm.CommandText = sqlQuery;
+                using (IDataReader reader = dbComm.ExecuteReader())
+                {
+                    //Reads row one then the other Reader reads the 2nd row
+                    reader.Read();
+                    reader.Read();
+
+                    //Treasury Consequence
+                    treasuryConsequence = reader.GetInt32(3);
+                    if (treasuryConsequence != 0)
+                    {
+                        statsClassReference.Treasury += treasuryConsequence;
+                    }
+                    Debug.Log(treasuryConsequence);
+                    //Happiness Consequence
+                    happinessConsequence = reader.GetInt32(4);
+                    if (happinessConsequence != 0)
+                    {
+                        statsClassReference.Happiness += happinessConsequence;
+                    }
+                    Debug.Log(happinessConsequence);
+                    //Healthiness Consequence
+                    healthinessConsequence = reader.GetInt32(5);
+                    if (healthinessConsequence != 0)
+                    {
+                        statsClassReference.Healthiness += healthinessConsequence;
+                    }
+                    Debug.Log(healthinessConsequence);
+                    //Infrastructure Consequence
+                    infrastructureConsequence = reader.GetInt32(6);
+                    if (infrastructureConsequence != 0)
+                    {
+                        statsClassReference.Infrastructure += infrastructureConsequence;
+                    }
+                    Debug.Log(infrastructureConsequence);
+                    reader.Close();
+                }
+                dbConn.Close();
+            }
+        }
+        GetEvent();
+        GetOptions();
+    }
+
+   
+
+
+
+}
---
+++ Assets/Scripts/GameScreen/Stats.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stats : MonoBehaviour {
+
+    //I want to have different stats in here which will be affected by the decisions and the colouring in at the begining of the game
+    //stats should be of Type Float since they will be changed around
+    //Stats will corresspond to different colours
+    /*
+     * Treasury = Yellow #FFF800FF
+     * Happiness = Orange #FF8E00FF
+     * Healthiness = Green #28FF00FF
+     * Infrastructure = Blue #1300FFFF
+     * 
+     * */
+    public ListsTesting eventsDatabase;
+
+    public Text treasuryText;
+    public Text happinessText;
+    public Text healthinessText;
+    public Text infrastructureText;
+
+    public float Treasury;
+    public float Happiness;
+    public float Healthiness;
+    public float Infrastructure;
+
+    // Use this for initialization
+    void Start () {
+
+        Treasury = 50;
+        Happiness = 50;
+        Healthiness = 50;
+        Infrastructure = 50;
+
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        treasuryText.text = Treasury.ToString();
+        happinessText.text = Happiness.ToString();
+        healthinessText.text = Healthiness.ToString();
+        infrastructureText.text = Infrastructure.ToString();
+
+
+        if (Treasury <= 0)
+        {
+            SceneManager.LoadScene("Game_Over");
+        }
+
+        else if (Healthiness <= 0)
+        {
+            SceneManager.LoadScene("Game_Over");
+
+        }
+
+        else if(Infrastructure <= 0)
+        {
+            SceneManager.LoadScene("Game_Over");
+        }
+
+        else if(Happiness <= 0)
+        {
+            SceneManager.LoadScene("Game_Over");
+        }
+
+        if (Treasury >= 100)
+        {
+            SceneManager.LoadScene("Overthrown");
+        }
+
+        else if (Healthiness >= 100)
+        {
+            SceneManager.LoadScene("Overthrown");
+
+        }
+
+        else if (Infrastructure >= 100)
+        {
+            SceneManager.LoadScene("Overthrown");
+        }
+
+        else if (Happiness >= 100)
+        {
+            SceneManager.LoadScene("Overthrown");
+        }
+
+    }
+}
